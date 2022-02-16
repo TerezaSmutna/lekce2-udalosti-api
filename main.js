536cd9bef3e20551924ba65a81ff605aff0a6634
LAPTOP-5LG8T5MU\terka <terka.kop@seznam.cz>
2022-02-16 16:10:31 +0100

    lesson 2 working

--- main.js
+++ main.js
@@ -1,2 +1,57 @@
 // tady je místo pro náš program
 console.log ("jsem nalinkovan")
+
+let nadpis = document.querySelector('h1');
+nadpis.classList.add('zluty');
+
+let ctverecek = document.querySelector('div.zluty')
+ctverecek.classList.add('ctverecek');
+
+let odstavec = document.querySelector('p');
+
+function ztucni() {
+ odstavec.style.fontWeight = "bold";
+}
+
+function ztenci() {
+  odstavec.style.fontWeight = "normal";
+}
+
+function zcerven() {
+  odstavec.classList.toggle('cerveny');
+}
+
+function zvets() {
+  //odstavec.classList.add("vetsi");
+  let neco = odstavec.style.fontSize
+  neco = "5px"
+  //odstavec.style.fontSize = "55px"
+  console.log(odstavec.style.fontSize);
+  //odstavec.style.fontSize = "1px"
+  //odstavec.style.fontSize = odstavec.style.fontSize + "1px"
+}
+
+function startAudio() {
+  let audioFile = document.getElementById('zvukovaStopa');
+  console.log('Zapínám píseň...');
+  audioFile.play();
+}
+function pauza () {
+  zvukovaStopa.pause();
+}
+
+function potichu() {
+  zvukovaStopa.volume = 0;
+}
+
+function akorat () {
+  zvukovaStopa.volume = 0.5;
+}
+
+function nahlas() {
+  zvukovaStopa.volume = 1;
+}
+ 
+function zacatek () {
+  zvukovaStopa.currentTime = 0;
+}
